753590105d6aa7d812b3a483bb028c193b94ca75
uzimith <uzimith.x9@gmail.com>
2015-06-17 14:06:56 +0900

    chapter1

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source "https://rubygems.org"
+
+gem 'rake'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'bcrypt'
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+end
---
+++ app.rb
@@ -0,0 +1,51 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra/json'
+
+require './models.rb'
+
+use Rack::Session::Cookie
+
+get '/' do
+  erb :index
+end
+
+# user management
+helpers do
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
+end
+
+get '/sign_in' do
+  erb :sign_in
+end
+
+get '/sign_up' do
+  erb :sign_up
+end
+
+get '/sign_out' do # for debug
+  session[:user_id] = nil
+  redirect '/'
+end
+
+post '/user/create' do
+  User.create(
+    name: params[:name],
+    password: params[:password],
+    password_confirmation: params[:password_confirmation]
+  )
+  redirect "/"
+end
+
+post '/session/create' do
+  user = User.find_by_name params[:name]
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+  end
+  redirect "/"
+end
